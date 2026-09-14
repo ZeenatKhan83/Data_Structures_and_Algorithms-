16ba424144bd52103522d8f39ca8b61f48d2d653
Zeenat Khan <zeenatkhan8378@gmail.com>
2026-09-14 17:52:00 +0530

    Add files via upload

--- pointer.cpp
+++ pointer.cpp
@@ -2,7 +2,10 @@
 using namespace std;
 int main(){
 	int x = 20;
+	float y = 3.14;
+	
 	int* p = &x;
+	float* q = &y;
 	
 	cout<<"Value of x: "<< x <<endl;
 	
@@ -12,5 +15,14 @@ int main(){
 	
 	cout<<"Value using pointer: "<< *p <<endl;
 	
+	cout<<endl;
+	
+	cout<<"Value of y: "<< y <<endl;
+	
+	cout<<"Address of y: "<< &y <<endl;
+	
+	cout<<"Value stored in pointer: "<< q <<endl;
+	
+	cout<<"Value using pointer: "<< *q <<endl;
 	return 0;
 }
