5fe27f9c79e969604f21552126069cff092f9db1
Zeenat Khan <zeenatkhan8378@gmail.com>
2026-09-17 23:56:41 +0530

    Add files via upload

--- pointer_array.cpp
+++ pointer_array.cpp
@@ -4,11 +4,20 @@ using namespace std;
 int main() {
 
     int arr[] = {10, 20, 30, 40, 50};
+    int n = 5;
 
     int* p = arr;
 
-    for (int i = 0; i < 5; i++) {
-        cout << "Element: " << *(p + i) << endl;
+    cout << "Array elements using pointer:\n\n";
+
+    for (int i = 0; i < n; i++) {
+
+        cout << "Index: " << i << endl;
+        cout << "Element: " << arr[i] << endl;
+        cout << "Address: " << &arr[i] << endl;
+        cout << "Value using pointer: " << *(p + i) << endl;
+
+        cout << "------------------------" << endl;
     }
 
     return 0;
